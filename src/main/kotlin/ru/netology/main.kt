b2efdacd70fcc26f273c9b57da4307b4706b2955
Alexey Potapov <aloy@bk.ru>
2022-03-15 17:19:13 +0300

    main logic for comission count in Vk pay

---
+++ src/main/kotlin/ru/netology/main.kt
@@ -0,0 +1,73 @@
+package ru.netology
+
+const val VKPAY = "Vk Pay"
+const val MMCARD = "mastercard & maestro"
+const val VISAMIR = "visa & mir"
+
+val lowFeeLimitMM = 300 * 100
+val hiFeeLimitMM = 75000 * 100
+val feeMM = 0.006
+val constAddMM = 20 * 100
+
+val feeVisaMir = 0.0075
+val minFeeVisaMir = 35 * 100
+
+val maxDayTransferCard = 150000 * 100
+val maxMonthTransferCard = 600000 * 100
+
+val maxTransferVK = 15000 * 100
+val maxMonthTransferVK = 40000 * 100
+
+fun main() {
+    outTransferFee(amount = 200 * 100)
+    outTransferFee(amount = 20000 * 100)
+    outTransferFee(amount = 10000 * 100, monthSum = 35000 * 100)
+    println();
+    outTransferFee(amount = 200 * 100, type = MMCARD)
+    outTransferFee(amount = 20000 * 100, monthSum = 20000 * 100, type = MMCARD)
+    outTransferFee(amount = 10000 * 100, monthSum = 595000 * 100, type = MMCARD)
+    outTransferFee(amount = 10000 * 100, daySum = 145000 * 100, type = MMCARD)
+    println();
+    outTransferFee(amount = 200 * 100, type = VISAMIR)
+    outTransferFee(amount = 20000 * 100, monthSum = 20000 * 100, type = VISAMIR)
+    outTransferFee(amount = 10000 * 100, monthSum = 595000 * 100, type = VISAMIR)
+    outTransferFee(amount = 10000 * 100, daySum = 145000 * 100, type = VISAMIR)
+
+}
+
+fun transferFee(type: String = VKPAY, monthSum: Int = 0, amount: Int): Int {
+    return when {
+        type == MMCARD && monthSum !in lowFeeLimitMM..hiFeeLimitMM -> (amount * feeMM).toInt() + constAddMM
+        type == VISAMIR -> if ((amount * feeVisaMir).toInt() >= minFeeVisaMir) (amount * feeVisaMir).toInt() else minFeeVisaMir
+        else -> 0
+    }
+}
+
+fun outTransferFee(type: String = VKPAY, monthSum: Int = 0, daySum: Int = 0, amount: Int) {
+    when {
+        type == VKPAY && amount > maxTransferVK -> {
+            println("Сумма перевода Vk превышает установленный лимит")
+        }
+        type == VKPAY && (monthSum + amount) > maxMonthTransferVK -> {
+            println("Сумма перевода Vk превышает лимит в месяц")
+        }
+        type !== VKPAY && (daySum + amount) > maxDayTransferCard -> {
+            println("Сумма перевода по карте превышает дневной лимит")
+        }
+        type !== VKPAY && (monthSum + amount) > maxMonthTransferCard -> {
+            println("Сумма перевода по карте превышает лимит в месяц")
+        }
+        else -> {
+            val transferFee = transferFee(type, monthSum, amount)
+            val transferFeeRub = transferFee / 100
+            val transferFeeKop = transferFee % 100
+            val amountRub = amount / 100
+            val amountKop = amount % 100
+            println("Вы совершатете перевод в системе $type")
+            println(
+                "При переводе $amountRub" + '\u20BD' + " $amountKop коп. " +
+                        "комиссия составит $transferFeeRub" + '\u20BD' + " $transferFeeKop коп."
+            )
+        }
+    }
+}
